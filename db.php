df926030f0cc8b2f3e1f42e908f744df9f19fce7
grosa82 <grosa82@gmail.com>
2015-05-17 20:01:57 -0600

    Add header and footer to db.php

--- db.php
+++ db.php
@@ -1,30 +1,39 @@
 <?php
+	include "header.php";
+	$servername = getenv('OPENSHIFT_MYSQL_DB_HOST');
+	$username = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+	$password = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+	$port = getenv('OPENSHIFT_MYSQL_DB_PORT');
+	$dbname = "php";
 
-$servername = getenv('OPENSHIFT_MYSQL_DB_HOST');
-$username = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
-$password = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
-$port = getenv('OPENSHIFT_MYSQL_DB_PORT');
-$dbname = "php";
+	// Create connection
 
-// Create connection
+	$conn = new mysqli($servername, $username, $password, $dbname, $port);
 
-$conn = new mysqli($servername, $username, $password, $dbname, $port);
+	// Check connection
+	if ($conn->connect_error) {
+	    die("Connection failed: " . $conn->connect_error);
+	}
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+	$sql = "SELECT id, name, email FROM user order by name";
+	$result = $conn->query($sql);
+?>
 
-$sql = "SELECT id, name, email FROM user order by name";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. " - Name: " . $row["name"]. " - Email: " . $row["email"]. "<br>";
-    }
-} else {
-    echo "0 results";
-}
-$conn->close();
-?> 
+<table border="1">
+<tbody>
+<?php
+	if ($result->num_rows > 0) {
+	    // output data of each row
+	    while($row = $result->fetch_assoc()) {
+	        echo "<tr><td>" . $row["id"]. "</td><td>" . $row["name"]. "</td><td>" . $row["email"]. "</td></tr>";
+	    }
+	} else {
+	    echo "0 results";
+	}
+	$conn->close();
+?>
+</tbody>
+</table> 
+<?php
+	include "footer.php";
+?>
