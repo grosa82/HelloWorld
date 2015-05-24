ac2b4d52c265dc6ff5d41957b16446ed52609c40
grosa82 <grosa82@gmail.com>
2015-05-23 19:06:21 -0600

    Add conflict finder

--- menu.php
+++ menu.php
@@ -1,5 +1,5 @@
 <li><a href="survey.php">PHP Survey</a></li>
-<li><a href="assignments.html">Assignment 02</a></li>
+<li><a href="schedule.php">Conflict Finder</a></li>
 <li><a href="assignments.html">Assignment 03</a></li>
 <li class="divider"></li>
 <li><a href="assignments.html">Assignment 04</a></li>
